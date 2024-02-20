1808d43ee0a0b3890ad469f882de0362428a383d
Rahuljain-tech <jainraahul03@gmail.com>
2024-02-21 00:24:56 +0530

    Added Comments

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import functools
 #     # Use a breakpoint in the code line below to debug your script.
 #     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
+# Q3 (a)
 def FINDMAX(a, b):
     # if a>b:
     #   return a
@@ -20,22 +20,22 @@ def FINDMAX(a, b):
     #    return b
     return max(a, b)
 
-
+# Q2
 def mul(n):
     return lambda a: a * n
 
-
+# Q1 (b)
 def positive_list(l):
     l1 = [2 * x for x in l]
     l2 = [x for x in l1 if x > 0]
     return l2
 
-
+# Q1 (a)
 def print_list(l):
     l1 = [2 * x for x in l]
     return l1
 
-
+# Q4 (a)
 def isPositive(n):
     if(n>0):
         
@@ -44,7 +44,9 @@ def isPositive(n):
     else:
         # return str(n) +(" is not a positive number")
         return False
-    
+
+
+# Q5 (a)    
 def multiply(a,b):
     return a*b    
 
@@ -57,11 +59,12 @@ if __name__ == '__main__':
     number = mul(3)
     print('Multiplication using lambda function is :- ', number(5))
     print('Max of two Numbers is : - ', FINDMAX(11, 32))
+    # Q3 (b)
     max_value = max(map(FINDMAX, l,l[1:]))
-    # # print(max(map(FINDMAX,l)))
     print(max_value)
     x = int(input("Enter a number :-"))
     print(isPositive(x))
+    # Q4 (b)
     filtered = filter(isPositive,l)
     print("All positive numbers in the list are : ")
     for i in filtered:
@@ -70,5 +73,8 @@ if __name__ == '__main__':
     a = int(input("Enter a : "))
     b = int(input("Enter b : "))
     print("Product of" "","" ,str(a) ,", " ,str(b), " is : ",multiply(a,b))   
+    
+
+    # Q5 (b)
     print("Product of elements of list is : ", functools.reduce(multiply,l)) 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
