9cab6fe1068224bacf7d4f874a3de76dea5a57ec
Rahuljain-tech <jainraahul03@gmail.com>
2024-02-21 11:26:02 +0530

    Level 2 completed

--- level2.py
+++ level2.py
@@ -14,8 +14,14 @@ def greet(s):
     else:
         return "Please select the correct language"
 
+def cal(n1 , n2=0):
+    return n1+n2 , n1-n2 , n1*n2 , n1/n2
+
 if __name__ == '__main__':
     n = int(input("E nter a number : "))
     print("Factorial of ",n," : ",calFactorial(n))
     s = str(input("Enter the language : "))
     print(greet(s))
+    in1 = int(input("Enter first no : "))
+    in2 = int(input("Enter second no : "))
+    print(cal(in1, in2))
